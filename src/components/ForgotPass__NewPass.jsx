0857f317fb50fb0e65baa45e96c2eacf18266bff
Kevin Perez <66193315+KevinPerez10@users.noreply.github.com>
2023-03-03 11:49:35 +0800

    password validation

--- src/components/ForgotPass__NewPass.jsx
+++ src/components/ForgotPass__NewPass.jsx
@@ -13,7 +13,7 @@ export default function ForgotPass__NewPass({open, onClose,props}) {
     password: newPassword
   };
   function changePass(){
-    if (password.length < MIN_PASSWORD_LENGTH) {
+    if (newPassword.length < MIN_PASSWORD_LENGTH) {
       setError("The password must contain at least 8 characters!")
     }
     else{
