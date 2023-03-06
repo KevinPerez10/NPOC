45d4887d36c413147b978a925f88bc9304c0387b
Kevin Perez <66193315+KevinPerez10@users.noreply.github.com>
2023-03-06 11:34:06 +0800

    kvn - changes to vite pt.2

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: './',
-  plugins: [react()],
-  esbuildTarget: 'es2020'
-})
+function defineConfig() {
+  return {
+    base: './',
+    plugins: [react()]
+  };
+}
+
+module.exports = { defineConfig };
